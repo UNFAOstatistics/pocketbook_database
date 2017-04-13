d6c5759a5ed08dcc184147c9be1f7307750b8d26
Markus Kainu <markuskainu@gmail.com>
2017-04-13 19:14:21 +0300

    Area harvested now properly sums the individual items. Matches now the manual calculation by Staceys for Bangladesh

--- code/manualdata_scripts/area_harvested_2015.R
+++ code/manualdata_scripts/area_harvested_2015.R
@@ -11,22 +11,22 @@ library(readr)
 #       exdir = "./input_data/raw/")
 # dat <- read_csv("./input_data/raw/Production_Crops_E_All_Data_(Norm).csv")
 dat <- read_csv("~/local_data/faostat/csv/production_crops_e_all_data_(normalized).csv")
-
-
 names(dat) <- tolower(str_replace_all(names(dat), " ", ""))
 
-dat$item <- as.character(dat$item)
+table_na(dat$item)
 
-dat1 <- dat[grepl("Total", dat$item),]
 
-df_area_harvested.df <- dat1 %>% filter(areacode < 5000, element %in% "Area harvested") %>% 
+df_area_harvested.df <- dat %>% 
+  mutate(item = as.character(item)) %>% 
+  # Lets keep only individual items by removing items flagged "A"
+  filter(!grepl("A", flag),
+         areacode < 5000,
+         element %in% "Area harvested") %>% 
   group_by(areacode,year) %>% 
-  dplyr::summarise(area_harvested = sum(value, na.rm=TRUE))
-
-names(df_area_harvested.df) <- c("FAOST_CODE","Year","area_harvested")
-
-df_area_harvested.df <- df_area_harvested.df[!duplicated(df_area_harvested.df[c("FAOST_CODE","Year")]),]
-df_area_harvested.df <- as.data.frame(df_area_harvested.df)
+  dplyr::summarise(area_harvested = sum(value, na.rm=TRUE)) %>% 
+  dplyr::rename(FAOST_CODE = areacode,
+         Year = year) %>% 
+  distinct(FAOST_CODE,Year, .keep_all = TRUE)  
 
 save(x = df_area_harvested.df, file = "./input_data/processed/df_area_harvested.RData")
 
